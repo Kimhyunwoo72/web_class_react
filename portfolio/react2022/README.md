396333c17da9a11c545f24c8d76a8ce4d90bad23
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-04-11 12:08:37 +0900

    0411

---
+++ portfolio/react2022/README.md
@@ -0,0 +1,5 @@
+# 리액트
+
+## 리액트 설치
+npx create-react-app react2022
+
---
+++ portfolio/react2022/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      리액트22
+    </div>
+  );
+}
+
+export default App;
